8cf73cbd4e11a90ef6b0a62203f6a0eea3c1ba13
Abdullah <abdullah.khalid@ems-uk.com>
2024-09-25 00:04:09 +0100

    Initial commit
    Please run the python file as a service. 
    A very simple version of pld.py file which checks pin 6 and when the UPS sends a signal the pi starts the shut down procedure.

---
+++ sd_x728.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+import os  # Import os to run system commands
+
+PLD_PIN = 6
+BUZZER_PIN = 20
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PLD_PIN, GPIO.IN)
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+while True:
+    i = GPIO.input(PLD_PIN)
+    if i == 0:
+        print("AC Power OK")
+        GPIO.output(BUZZER_PIN, 0)
+    elif i == 1:
+        print("Power Supply A/C Lost")
+        GPIO.output(BUZZER_PIN, 1)
+        time.sleep(1)  # Small delay before shutdown
+        print("Shutting down the system...")
+        os.system("sudo shutdown now")  # Shutdown command
+        break  # Exit the loop after shutdown command
+        
+    time.sleep(1)
